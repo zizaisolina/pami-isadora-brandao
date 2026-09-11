179892b57e6b9a9fd717b1f6f827feae2200f616
Ziza Ribeiro <zizaisolina@gmail.com>
2026-09-10 21:56:24 -0300

    Aula sobre funções

---
+++ aula02/funcoes.js
@@ -0,0 +1,33 @@
+// function MultiplicaNumeros() {
+//     let n1 = 2;
+//     let n2 = 3;
+
+//     console.log(n1 * n2);
+//     // + (soma)   - (subtração)
+//     // * (multiplicação)  / (divisão)
+//     // ^ (potência)  % (sobra de devisão)
+// }
+
+// MultiplicaNumeros () ;
+
+//     function MultiplicaComParametros (v1, v2) {
+//         console.log(v1, " x " , v2, " = ", v1 * v2);
+//     }
+
+// MultiplicaComParametros(3, 4);
+// MultiplicaComParametros(4, 10);
+// MultiplicaComParametros(6, 7);
+
+// function EquacaoDeBurrinho(x, y, z) {
+//     let resultado = (x + y) / z; 
+//     return resultado;
+// }
+
+// console.log(EquacaoDeBurrinho(2,3,5));
+
+
+const EquacaoDeTontinhos = (x, y, z, w) => {
+    return (x+y)*(z+w)/2 ;
+}
+
+console.log("O resultado é: ", EquacaoDeTontinho(3,5,8,13));
